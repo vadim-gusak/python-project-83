e57d8637e8db8378c6f1767b2e22fe8cb8a9f85c
Vadim Gusak <gusakvd@gmail.com>
2022-12-16 11:24:05 +1000

    create table url_checks

--- database.sql
+++ database.sql
@@ -4,3 +4,12 @@ CREATE TABLE urls (
     name varchar(255),
     created_at date
 );
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id) NOT NULL,
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at date
+);
